6a7fbef0dbccb94f09e1011175ef6bc0a91261f1
sujithapathapati <sujithapathapati678@gmail.com>
2026-06-21 21:57:54 +0530

    Added Exercise 6

---
+++ Design Principles and Patterns/Exercise-6-ProxyPattern/ProxyPatternTest.java
@@ -0,0 +1,13 @@
+public class ProxyPatternTest {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("sample.jpg");
+
+        System.out.println("First Call:");
+        image.display();
+
+        System.out.println("\nSecond Call:");
+        image.display();
+    }
+}
