47faf6f6c5874402f2072a72e419f0cd524d954b
sujithapathapati <sujithapathapati678@gmail.com>
2026-06-23 12:20:43 +0530

    Added JUnit Testing Exercise 1

---
+++ Week-1/JUnit Testing/Exercise-1-SettingUpJUnit/src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
