4933700c3d2dc23b214b63c12a90435ab2e3b104
sujithapathapati <sujithapathapati678@gmail.com>
2026-06-27 10:41:26 +0530

    Create Week-2 folder

---
+++ Week-2/Mockito Testing/Exercise-1-MockingAndStubbing/src/test/java/MyServiceTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service = new MyService(mockApi);
+
+        String result = service.fetchData();
+
+        assertEquals("Mock Data", result);
+    }
+}
